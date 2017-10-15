d70b402aa918db4c941a3394ad7a627cc3b985e9
卢悦明 <luyueming@YM-MacBook-Pro.local>
2017-10-15 09:47:59 +0800

    Initial Commit

---
+++ Git test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git test
+//
+//  Created by 卢悦明 on 2017/10/15.
+//  Copyright © 2017年 卢悦明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git test
+//
+//  Created by 卢悦明 on 2017/10/15.
+//  Copyright © 2017年 卢悦明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
